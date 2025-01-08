f84173e2bc05844473aae964cedae7f3f7465b24
bakytzamalzorobekova <shamuratovna89@gmail.com>
2025-01-08 17:54:21 +0300

    Task oop

--- dreamteam/src/main/java/ru/devguru/practices/chapter_1_jama/oop/manager/ResourceManager.java
+++ dreamteam/src/main/java/ru/devguru/practices/chapter_1_jama/oop/manager/ResourceManager.java
@@ -10,30 +10,24 @@ import java.util.List;
 
 public class ResourceManager {
 
-    private static List<Resource> resources;
+    private static List<Resource> resources = new ArrayList<>();
+
+    private ResourceManager() {
+        resources = new ArrayList<>();
+    }
 
     public static void addResource(Resource resource) {
-        if (resources == null) {
-            resources = new ArrayList<>();
-        }
-
-        for (Resource res : resources) {
-            if (resource.equals(res)) {
-                res.setAmount(resource.getAmount() + res.getAmount());
-                return;
-            }
-        }
-
         resources.add(resource);
     }
 
-    public static Resource findResource(String resourceName, List<Resource> resources) {
+    public static void findResource(String resourceName, List<Resource> resources) {
         for (Resource resource : resources) {
             if (resource.getName().equals(resourceName)) {
-                return resource;
+                System.out.println("Ресурс:  " + resource + " найден");
+                return;
             }
         }
-        return null;
+        System.out.println("Ресурс: " + resourceName + "' не найден.");
     }
 
     public static void displayAllResources(List<Resource> resources) {
@@ -45,33 +39,31 @@ public class ResourceManager {
     public static void main(String[] args) {
 
         // создание ресурсов
-        EnergyResource energy = new EnergyResource("energy", 57.5, 220.0);
+        Resource energy = new EnergyResource("energy", 57.5, 220.0);
         Resource fuel = new FuelResource("fuel", 93.3, "oil", List.of("One", "Two", "Three"));
         Resource water = new WaterResource("water", 66.6, 55.0);
         Resource oxygen = new OxygenResource("oxygen", 96.0, 30.0);
 
         // добавление ресурсов в список
-        List<Resource> resourceList = new ArrayList<>();
-        resourceList.add(energy);
-        resourceList.add(fuel);
-        resourceList.add(water);
-        resourceList.add(oxygen);
+        addResource(energy);
+        addResource(fuel);
+        addResource(water);
+        addResource(oxygen);
 
         // отображение всех ресурсов
-        displayAllResources(resourceList);
+        displayAllResources(resources);
 
-        // добавление ресурса
+        // добавление нового ресурса
         System.out.println("Добавить ресурс");
-        Resource resource = new OxygenResource("oxygen", 96.0, 30.0);
-        resource.setAmount(20.0);
+        Resource resource = new OxygenResource("newWater", 20.0, 30.0);
         addResource(resource);
 
-        // отображение всех ресурсов для проверки
-        displayAllResources(resourceList);
+        // отображение всех ресурсов после добавления нового ресурса
+        displayAllResources(resources);
 
         // поиск ресурса по названию
-        System.out.println("Поиск ресурса  по названию");
-        System.out.println(findResource("oxygen", resourceList));
+        System.out.println("Поиск ресурса по названию");
+        findResource("oxygen", resources);
 
         // создание подсистем используя полиморфизм
         System.out.println("Подсистемы");
--- dreamteam/src/main/java/ru/devguru/practices/chapter_1_jama/oop/resources/Resource.java
+++ dreamteam/src/main/java/ru/devguru/practices/chapter_1_jama/oop/resources/Resource.java
@@ -25,7 +25,6 @@ public abstract class Resource {
 
     public abstract void displayDetails();
 
-
     @Override
     public String toString() {
         return "Resource{" +
